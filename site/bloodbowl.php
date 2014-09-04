da1d8700e3d7dc047683a199074d3a90178c79c2
Cosworth32 <keld@hjortskov.net>
2014-09-04 13:03:30 +0200

    #13 fix for "DS"
    DS is removed from Joomla 3

--- site/bloodbowl.php
+++ site/bloodbowl.php
@@ -1,14 +1,16 @@
 ﻿<?php
 // No direct access to this file
 defined('_JEXEC') or die;
-
+if(!defined('DS')){ 
+define('DS',DIRECTORY_SEPARATOR);
+}
 // require helper file 
 JLoader::register('BloodBowlHelper', dirname(__FILE__) . DS . 'helpers' . DS . 'bloodbowl.php');
 
 // import joomla controller library
 jimport('joomla.application.component.controller');
  
-//JTable::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR.DS.'tables');
+//JTable::addIncludePath(JPATH_COMPONENT_ADMINISTRATOR. DS .'tables');
 
 // Get an instance of the controller prefixed by BloodBowl
 $controller = JController::getInstance('BloodBowl');
